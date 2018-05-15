0865dfec5a2b182bbd1ef460655910fa7305f42b
frodoman <my_adress_2003@hotmail.com>
2018-05-15 23:28:54 +0100

    Add files for presenter and view protocol.

---
+++ Moviee/SourceCodes/Presenters/MVMoviePresenter.swift
@@ -0,0 +1,15 @@
+//
+//  MVMoviePresenter.swift
+//  Moviee
+//
+//  Created by Xinghou Liu on 15/05/2018.
+//  Copyright © 2018 Xmart Soft Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MVMoviePresenter: NSObject {
+    var targetView: MVMovieViewProtocol? = nil
+    
+    
+}
---
+++ Moviee/SourceCodes/Presenters/MVMovieViewProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  MVMovieViewProtocol.swift
+//  Moviee
+//
+//  Created by Xinghou Liu on 15/05/2018.
+//  Copyright © 2018 Xmart Soft Ltd. All rights reserved.
+//
+
+import Foundation
+
+protocol MVMovieViewProtocol {
+    func startWaiting()
+    func stopWaiting()
+    func showMovies()
+    func showMovieDetails()
+    func showCollectionMovies()
+}
