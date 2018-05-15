3cbac200ebf17452ed4f1f229b85a4d687f28694
Xinghou.Liu <xliu147@ford.com>
2018-05-15 16:21:19 +0100

    Add const for URLs and API key.

---
+++ Moviee/SourceCodes/Models/Consts.swift
@@ -0,0 +1,17 @@
+//
+//  Consts.swift
+//  Moviee
+//
+//  Created by Xinghou.Liu on 15/05/2018.
+//  Copyright © 2018 Xmart Soft Ltd. All rights reserved.
+//
+
+import Foundation
+
+let ConstAPIKey = "ad358c8a9834dcfc74af82859aa03a62"
+let ConstUrlTextNowPlaying = "https://api.themoviedb.org/3/movie/now_playing?api_key="+ConstAPIKey+"&language=en-US&page=1"
+let ConstUrlTextMovieDetails = ""
+let ConstUrlTextCollection = ""
+
+let ConstUrlTextImageDomain = "https://image.tmdb.org/t/p/w500/"
+
